48a51fe1a328ae0890afaa8f185125c322a18b39
Kevin Cox <kevin.cox.work@gmail.com>
2020-06-28 15:06:30 -0400

    fixed wording

--- src/constants/jobs/manulife.js
+++ src/constants/jobs/manulife.js
@@ -6,7 +6,7 @@ export const manulife2020 = {
   jobDesc: [
     'Confidently lead a team of co-ops and provided solution architectural guidance for new and existing projects which were managed using Agile methodology and tracked with Atlassian’s JIRA',
     'Interfaced with a plethora of internal and external REST/SOAP API’s to eliminate all manual effort required for operation (Microsoft Graph, Survey Monkey, Planview)',
-    'Worked collaboratively alongside team members in all aspects of the solution, from database design to incorporating UI/UX methodology, to create robust, containerized MERN stack and C# applications',
+    'Worked collaboratively alongside team members in all aspects of the solution, from database design to incorporating UI/UX methodologies, to create robust, containerized MERN stack and C# applications',
     'Drastically increased performance of existing applications by thoroughly analyzing and then improving network activity, code structure and the pipeline process',
   ],
 };
